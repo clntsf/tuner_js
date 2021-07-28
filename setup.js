d518d0e38045e5c1b40d92691d018dd5084dd540
ctsf1 <79168898+ctsf1@users.noreply.github.com>
2021-07-28 15:33:41 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,24 +1,36 @@
-function toggleCheckboxesEnabled(){    
-    const boxes = document.querySelectorAll('.note-input');
-    for (n in boxes) boxes[n].disabled = !(boxes[n].disabled);
+function changeFileText(promise_output){file_str = promise_output}
+
+function freqForceCheckboxChange(checkbox_checked){
+    // Show or hide notes grid and label
+    if (checkbox_checked) tunetypes_container.style.display = "block";
+    else tunetypes_container.style.display = "none";
 }
 
-function toggleNotesTuning(){
-    // Show or hide notes grid and label
+// Shows or hides tunetype selectors based on radiobutton selection
+tunetypeRadioChange = function(radio_value){
 
-    if (note_div.display == "none") {note_div.display="grid"; note_div_label.hidden = false}
-    else {note_div.display = "none"; note_div_label.hidden = true}
+    if (radio_value === "1") {
+        note_selector.style.display = "block";
+        scale_selector.style.display = "none";
+        tunetype="notes";
+    }
+    else {
+        note_selector.style.display = "none";
+        scale_selector.style.display="block";
+        tunetype="scale";
+    }
 }
 
 function setup(){
-    
+
+    default_scale = [0,2,4,5,7,9,11]
     download_button = document.getElementById("download-button");
-    download_button.addEventListener("click", outputZipFile,false);
 
+    tunetypes_container = document.getElementById('tunetype-variants');
+    note_selector = document.getElementById("notes-tunetype-container");
+    scale_selector = document.getElementById("scales-tunetype-container");
+
+    tunetype="notes"; file_str=''; filename=''
     tune_freqs = document.getElementById('tune-freqs');
-    note_div = document.getElementById('scale-notes').style;
-    note_div_label = document.getElementById('notes-label')
-
-    tune_freqs.addEventListener("change", toggleNotesTuning);
 
 }
